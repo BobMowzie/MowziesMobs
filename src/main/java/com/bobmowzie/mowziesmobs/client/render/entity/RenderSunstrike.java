e4220fac4ca222e8cae967226423be04dd9cccc7
Paul Fulham <paul.fulham0@gmail.com>
2015-11-27 17:18:13 -0800

    makes scorch only render on top of blocks

--- src/main/java/com/bobmowzie/mowziesmobs/client/render/entity/RenderSunstrike.java
+++ src/main/java/com/bobmowzie/mowziesmobs/client/render/entity/RenderSunstrike.java
@@ -1,12 +1,18 @@
 package com.bobmowzie.mowziesmobs.client.render.entity;
 
-import com.bobmowzie.mowziesmobs.MowziesMobs;
-import com.bobmowzie.mowziesmobs.common.entity.EntitySunstrike;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.MathHelper;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+
 import org.lwjgl.opengl.GL11;
+
+import com.bobmowzie.mowziesmobs.MowziesMobs;
+import com.bobmowzie.mowziesmobs.common.entity.EntitySunstrike;
 
 public class RenderSunstrike extends Render
 {
@@ -46,6 +52,14 @@ public class RenderSunstrike extends Render
 
     private static final double LINGER_RADIUS = 1.2;
 
+	private static final double SCORCH_MIN_U = 11 * RING_FRAME_SIZE / TEXTURE_WIDTH;
+
+	private static final double SCORCH_MAX_U = SCORCH_MIN_U + RING_FRAME_SIZE / TEXTURE_WIDTH;
+
+	private static final double SCORCH_MIN_V = RING_FRAME_SIZE / TEXTURE_HEIGHT;
+
+	private static final double SCORCH_MAX_V = SCORCH_MIN_V + RING_FRAME_SIZE / TEXTURE_HEIGHT;
+
     @Override
     public void doRender(Entity entity, double x, double y, double z, float yaw, float delta)
     {
@@ -72,22 +86,74 @@ public class RenderSunstrike extends Render
 
     private void drawLingering(EntitySunstrike sunstrike, float delta)
     {
-        double minU = 11 * RING_FRAME_SIZE / TEXTURE_WIDTH;
-        double maxU = minU + RING_FRAME_SIZE / TEXTURE_WIDTH;
-        double minV = RING_FRAME_SIZE / TEXTURE_HEIGHT;
-        double maxV = minV + RING_FRAME_SIZE / TEXTURE_HEIGHT;
-        double offset = PIXEL_SCALE * LINGER_RADIUS - 0.1;
         setupGL();
         bindEntityTexture(sunstrike);
+        drawScorch(sunstrike, delta);
+        revertGL();
+    }
+
+    private void drawScorch(EntitySunstrike sunstrike, float delta)
+    {
+        World world = renderManager.worldObj;
+        double ex = sunstrike.lastTickPosX + (sunstrike.posX - sunstrike.lastTickPosX) * delta;
+        double ey = sunstrike.lastTickPosY + (sunstrike.posY - sunstrike.lastTickPosY) * delta + sunstrike.getShadowSize();
+        double ez = sunstrike.lastTickPosZ + (sunstrike.posZ - sunstrike.lastTickPosZ) * delta;
+        int minX = MathHelper.floor_double(ex - LINGER_RADIUS);
+        int maxX = MathHelper.floor_double(ex + LINGER_RADIUS);
+        int minY = MathHelper.floor_double(ey - LINGER_RADIUS);
+        int maxY = MathHelper.floor_double(ey);
+        int minZ = MathHelper.floor_double(ez - LINGER_RADIUS);
+        int maxZ = MathHelper.floor_double(ez + LINGER_RADIUS);
         Tessellator t = Tessellator.instance;
         t.startDrawingQuads();
-        t.setColorRGBA_F(1, 1, 1, 0.7f);
-        t.addVertexWithUV(-LINGER_RADIUS + offset, -0.01, -LINGER_RADIUS + offset, minU, minV);
-        t.addVertexWithUV(-LINGER_RADIUS + offset, -0.01, LINGER_RADIUS + offset, minU, maxV);
-        t.addVertexWithUV(LINGER_RADIUS + offset, -0.01, LINGER_RADIUS + offset, maxU, maxV);
-        t.addVertexWithUV(LINGER_RADIUS + offset, -0.01, -LINGER_RADIUS + offset, maxU, minV);
+        for (int bx = minX; bx <= maxX; bx++)
+        {
+            for (int by = minY; by <= maxY; by++)
+            {
+                for (int bz = minZ; bz <= maxZ; bz++)
+                {
+                    Block block = world.getBlock(bx, by - 1, bz);
+
+                    if (block.getMaterial() != Material.air && world.getBlockLightValue(bx, by, bz) > 3)
+                    {
+                        drawScorchBlock(block, bx, by, bz, ex, ey - sunstrike.getShadowSize(), ez);
+                    }
+                }
+            }
+        }
+        GL11.glDepthMask(false);
         t.draw();
-        revertGL();
+        GL11.glDepthMask(true);
+    }
+
+    private void drawScorchBlock(Block block, int bx, int by, int bz, double ex, double ey, double ez)
+    {
+        Tessellator t = Tessellator.instance;
+        if (block.renderAsNormalBlock())
+        {
+            float opacity = (float) ((1 - (ey - by) / 2) / 2 * renderManager.worldObj.getLightBrightness(bx, by, bz));
+            if (opacity >= 0)
+            {
+                if (opacity > 1)
+                {
+                    opacity = 1;
+                }
+                t.setColorRGBA_F(1, 1, 1, opacity);
+                double minX = bx + block.getBlockBoundsMinX() - ex;
+                double maxX = bx + block.getBlockBoundsMaxX() - ex;
+                double y = by + block.getBlockBoundsMinY() - ey + 0.015625;
+                double minZ = bz + block.getBlockBoundsMinZ() - ez;
+                double maxZ = bz + block.getBlockBoundsMaxZ() - ez;
+                double minU = (-minX / 2 / LINGER_RADIUS + 0.5) * (SCORCH_MAX_U - SCORCH_MIN_U) + SCORCH_MIN_U;
+                double maxU = (-maxX / 2 / LINGER_RADIUS + 0.5) * (SCORCH_MAX_U - SCORCH_MIN_U) + SCORCH_MIN_U;
+                double minV = (-minZ / 2 / LINGER_RADIUS + 0.5) * (SCORCH_MAX_V - SCORCH_MIN_V) + SCORCH_MIN_V;
+                double maxV = (-maxZ / 2 / LINGER_RADIUS + 0.5) * (SCORCH_MAX_V - SCORCH_MIN_V) + SCORCH_MIN_V;
+                t.addVertexWithUV(minX, y, minZ, minU, minV);
+                t.addVertexWithUV(minX, y, maxZ, minU, maxV);
+                t.addVertexWithUV(maxX, y, maxZ, maxU, maxV);
+                t.addVertexWithUV(maxX, y, minZ, maxU, minV);
+            }
+        }
     }
 
     private void drawStrike(EntitySunstrike sunstrike, double maxY, float delta)
