031f6721c9feddf11d399394926c42f44f269fef
BobMowzie <jnad325@gmail.com>
2016-05-18 13:00:58 -0400

    Incremented version number

--- src/main/java/com/bobmowzie/mowziesmobs/MowziesMobs.java
+++ src/main/java/com/bobmowzie/mowziesmobs/MowziesMobs.java
@@ -35,7 +35,7 @@ import net.minecraftforge.common.MinecraftForge;
 public class MowziesMobs {
     public static final String MODID = "mowziesmobs";
     public static final String NAME = "Mowzie's Mobs";
-    public static final String VERSION = "1.2.5";
+    public static final String VERSION = "1.2.8";
     public static final String LLIBRARY_VERSION = "1.3.1";
     public static final String DEPENDENCIES = "required-after:llibrary@[" + MowziesMobs.LLIBRARY_VERSION + ",)";
 
