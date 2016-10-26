541a4d52d43d94ec3aa51169e7ef50b88940c22a
pau101 <paul.fulham0@gmail.com>
2016-10-26 16:06:18 -0700

    Update LLibrary version

--- src/main/java/com/bobmowzie/mowziesmobs/MowziesMobs.java
+++ src/main/java/com/bobmowzie/mowziesmobs/MowziesMobs.java
@@ -36,7 +36,7 @@ public class MowziesMobs {
     public static final String MODID = "mowziesmobs";
     public static final String NAME = "Mowzie's Mobs";
     public static final String VERSION = "1.3.0";
-    public static final String LLIBRARY_VERSION = "1.5.1";
+    public static final String LLIBRARY_VERSION = "1.7.1";
     public static final String DEPENDENCIES = "required-after:llibrary@[" + MowziesMobs.LLIBRARY_VERSION + ",)";
 
     @Instance(MowziesMobs.MODID)
