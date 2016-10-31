046dec2558ed7d514e91d8ca0c4674a71ecc2b19
pau101 <paul.fulham0@gmail.com>
2016-10-30 22:08:49 -0700

    Fix dart rendering

--- src/main/java/com/bobmowzie/mowziesmobs/client/render/entity/DartRenderer.java
+++ src/main/java/com/bobmowzie/mowziesmobs/client/render/entity/DartRenderer.java
@@ -1,5 +1,10 @@
 package com.bobmowzie.mowziesmobs.client.render.entity;
 
+import org.lwjgl.opengl.GL11;
+
+import com.bobmowzie.mowziesmobs.MowziesMobs;
+import com.bobmowzie.mowziesmobs.server.entity.EntityDart;
+
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.VertexBuffer;
@@ -11,12 +16,6 @@ import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL12;
-
-import com.bobmowzie.mowziesmobs.MowziesMobs;
-import com.bobmowzie.mowziesmobs.server.entity.EntityDart;
-
 @SideOnly(Side.CLIENT)
 public class DartRenderer extends Render<EntityDart> {
     private static final ResourceLocation TEXTURE = new ResourceLocation(MowziesMobs.MODID, "textures/entity/textureDart.png");
@@ -25,7 +24,6 @@ public class DartRenderer extends Render<EntityDart> {
         super(mgr);
     }
 
-
     @Override
     protected ResourceLocation getEntityTexture(EntityDart entity) {
         return DartRenderer.TEXTURE;
@@ -33,61 +31,56 @@ public class DartRenderer extends Render<EntityDart> {
 
     @Override
     public void doRender(EntityDart dart, double x, double y, double z, float yaw, float delta) {
-        this.bindEntityTexture(dart);
+        bindEntityTexture(dart);
+        GlStateManager.color(1, 1, 1);
         GlStateManager.pushMatrix();
         GlStateManager.translate(x, y, z);
-        GlStateManager.rotate(dart.prevRotationYaw + (dart.rotationYaw - dart.prevRotationYaw) * delta - 90.0F, 0.0F, 1.0F, 0.0F);
-        GlStateManager.rotate(dart.prevRotationPitch + (dart.rotationPitch - dart.prevRotationPitch) * delta, 0.0F, 0.0F, 1.0F);
+        GlStateManager.rotate(dart.prevRotationYaw + (dart.rotationYaw - dart.prevRotationYaw) * delta - 90, 0, 1, 0);
+        GlStateManager.rotate(dart.prevRotationPitch + (dart.rotationPitch - dart.prevRotationPitch) * delta, 0, 0, 1);
         Tessellator tes = Tessellator.getInstance();
         VertexBuffer buf = tes.getBuffer();
-        byte b0 = 0;
-        float f2 = 0.0F;
-        float f3 = 0.5F;
-        float f4 = (b0 * 10) / 32.0F;
-        float f5 = (5 + b0 * 10) / 32.0F;
-        float f6 = 0.0F;
-        float f7 = 0.15625F;
-        float f8 = (5 + b0 * 10) / 32.0F;
-        float f9 = (10 + b0 * 10) / 32.0F;
-        float f10 = 0.05625F;
         GlStateManager.enableRescaleNormal();
-        float f11 = dart.arrowShake - delta;
-
-        if (f11 > 0.0F) {
-            float f12 = -MathHelper.sin(f11 * 3.0F) * f11;
-            GlStateManager.rotate(f12, 0.0F, 0.0F, 1.0F);
+        float shake = dart.arrowShake - delta;
+        if (shake > 0) {
+            GlStateManager.rotate(-MathHelper.sin(shake * 3) * shake, 0, 0, 1);
         }
-
-        GlStateManager.rotate(45.0F, 1.0F, 0.0F, 0.0F);
-        GlStateManager.scale(f10, f10, f10);
-        GlStateManager.translate(-4.0F, 0.0F, 0.0F);
-        GlStateManager.glNormal3f(f10, 0.0F, 0.0F);
+        GlStateManager.rotate(45, 1, 0, 0);
+        GlStateManager.scale(0.05625F, 0.05625F, 0.05625F);
+        GlStateManager.translate(-4, 0, 0);
+        if (renderOutlines) {
+            GlStateManager.enableColorMaterial();
+            GlStateManager.enableOutlineMode(getTeamColor(dart));
+        }
+        GlStateManager.glNormal3f(0.05625F, 0, 0);
         buf.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
-        buf.pos(-7.0D, -2.0D, -2.0D).tex(f6, f8).endVertex();
-        buf.pos(-7.0D, -2.0D, 2.0D).tex(f7, f8).endVertex();
-        buf.pos(-7.0D, 2.0D, 2.0D).tex(f7, f9).endVertex();
-        buf.pos(-7.0D, 2.0D, -2.0D).tex(f6, f9).endVertex();
+        buf.pos(-7, -2, -2).tex(0, 0.15625D).endVertex();
+        buf.pos(-7, -2, 2).tex(0.15625D, 0.15625D).endVertex();
+        buf.pos(-7, 2, 2).tex(0.15625D, 0.3125D).endVertex();
+        buf.pos(-7, 2, -2).tex(0, 0.3125D).endVertex();
         tes.draw();
-        GlStateManager.glNormal3f(-f10, 0.0F, 0.0F);
+        GlStateManager.glNormal3f(-0.05625F, 0, 0);
         buf.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
-        buf.pos(-7.0D, 2.0D, -2.0D).tex(f6, f8).endVertex();
-        buf.pos(-7.0D, 2.0D, 2.0D).tex(f7, f8).endVertex();
-        buf.pos(-7.0D, -2.0D, 2.0D).tex(f7, f9).endVertex();
-        buf.pos(-7.0D, -2.0D, -2.0D).tex(f6, f9).endVertex();
+        buf.pos(-7, 2, -2).tex(0, 0.15625D).endVertex();
+        buf.pos(-7, 2, 2).tex(0.15625D, 0.15625D).endVertex();
+        buf.pos(-7, -2, 2).tex(0.15625D, 0.3125D).endVertex();
+        buf.pos(-7, -2, -2).tex(0, 0.3125D).endVertex();
         tes.draw();
-
         for (int i = 0; i < 4; ++i) {
-            GlStateManager.rotate(90.0F, 1.0F, 0.0F, 0.0F);
-            GlStateManager.glNormal3f(0.0F, 0.0F, f10);
+            GlStateManager.rotate(90, 1, 0, 0);
+            GlStateManager.glNormal3f(0, 0, 0.05625F);
             buf.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
-            buf.pos(-8.0D, -2.0D, 0.0D).tex(f2, f4).endVertex();
-            buf.pos(8.0D, -2.0D, 0.0D).tex(f3, f4).endVertex();
-            buf.pos(8.0D, 2.0D, 0.0D).tex(f3, f5).endVertex();
-            buf.pos(-8.0D, 2.0D, 0.0D).tex(f2, f5).endVertex();
+            buf.pos(-8, -2, 0).tex(0, 0).endVertex();
+            buf.pos(8, -2, 0).tex(0.5D, 0).endVertex();
+            buf.pos(8, 2, 0).tex(0.5D, 0.15625D).endVertex();
+            buf.pos(-8, 2, 0).tex(0, 0.15625D).endVertex();
             tes.draw();
         }
-
+        if (renderOutlines) {
+            GlStateManager.disableOutlineMode();
+            GlStateManager.disableColorMaterial();
+        }
         GlStateManager.disableRescaleNormal();
         GlStateManager.popMatrix();
+        super.doRender(dart, x, y, z, yaw, delta);
     }
 }
