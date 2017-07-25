c0ed26f2517bbbca44fe8523f22e423cce3cacdd
BobMowzie <jnad325@gmail.com>
2017-07-24 20:38:52 -0400

    Oops

--- src/main/java/com/bobmowzie/mowziesmobs/server/entity/foliaath/EntityBabyFoliaath.java
+++ src/main/java/com/bobmowzie/mowziesmobs/server/entity/foliaath/EntityBabyFoliaath.java
@@ -105,7 +105,7 @@ public class EntityBabyFoliaath extends MowzieEntity {
 
         //Growing
         if (!world.isRemote) {
-            if (/*ticksExisted % 20 == 0 && */!getHungry()) {
+            if (ticksExisted % 20 == 0 && !getHungry()) {
                 incrementGrowth();
             }
             // TODO: cleanup this poor logic
