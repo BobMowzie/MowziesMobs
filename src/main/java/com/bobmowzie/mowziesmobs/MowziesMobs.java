bcc83fc97eeeb78678036c884f4e924bafc94761
BobMowzie <jnad325@gmail.com>
2018-05-21 17:31:52 -0400

    Correct LLibrary version

--- src/main/java/com/bobmowzie/mowziesmobs/MowziesMobs.java
+++ src/main/java/com/bobmowzie/mowziesmobs/MowziesMobs.java
@@ -35,7 +35,7 @@ public final class MowziesMobs {
     public static final String MODID = "mowziesmobs";
     public static final String NAME = "Mowzie's Mobs";
     public static final String VERSION = "1.4.1";
-    public static final String LLIBRARY_VERSION = "1.7.10";
+    public static final String LLIBRARY_VERSION = "1.7.9";
     public static final String DEPENDENCIES = "required-after:llibrary@[" + MowziesMobs.LLIBRARY_VERSION + ",)";
 
     private static final class Holder {
