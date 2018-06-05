1b91c154c1b1a7df20d7977371fc990bedb0bcac
BobMowzie <jnad325@gmail.com>
2018-06-05 14:44:21 -0400

    Removes prints

--- src/main/java/com/bobmowzie/mowziesmobs/server/entity/barakoa/EntityBarakoanToPlayer.java
+++ src/main/java/com/bobmowzie/mowziesmobs/server/entity/barakoa/EntityBarakoanToPlayer.java
@@ -38,9 +38,9 @@ public class EntityBarakoanToPlayer extends EntityBarakoan<EntityPlayer> {
 
     @Override
     protected void removeAsPackMember() {
-        System.out.println(getPlayerProps().getPackSize());
+//        System.out.println(getPlayerProps().getPackSize());
         getPlayerProps().removePackMember(this);
-        System.out.println(getPlayerProps().getPackSize());
+//        System.out.println(getPlayerProps().getPackSize());
     }
 
     private MowziePlayerProperties getPlayerProps() {
