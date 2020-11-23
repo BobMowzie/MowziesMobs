05c826f598cfb8009379761add5be2b2b278a188
BobMowzie <jnad325@gmail.com>
2020-11-22 19:52:39 -0500

    Fix blocks

--- src/main/java/com/bobmowzie/mowziesmobs/server/block/BlockHandler.java
+++ src/main/java/com/bobmowzie/mowziesmobs/server/block/BlockHandler.java
@@ -3,7 +3,9 @@ package com.bobmowzie.mowziesmobs.server.block;
 import com.bobmowzie.mowziesmobs.MowziesMobs;
 import net.minecraft.block.Block;
 import net.minecraft.block.SlabBlock;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,7 +14,7 @@ public final class BlockHandler {
     private BlockHandler() {}
 
     public static final DeferredRegister<Block> REG = new DeferredRegister<>(ForgeRegistries.BLOCKS, MowziesMobs.MODID);
-    public static final RegistryObject<Block> PAINTED_ACACIA = REG.register("painted_acacia", () -> new Block(Block.Properties.create(Material.WOOD)));
+    public static final RegistryObject<Block> PAINTED_ACACIA = REG.register("painted_acacia", () -> new Block(Block.Properties.create(Material.WOOD, MaterialColor.ADOBE).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
     public static final RegistryObject<SlabBlock> PAINTED_ACACIA_SLAB = REG.register("painted_acacia_slab", () -> new SlabBlock(Block.Properties.from(PAINTED_ACACIA.get())));
     public static final RegistryObject<BlockGrottol> GROTTOL = REG.register("grottol", () -> new BlockGrottol(Block.Properties.create(Material.ROCK).noDrops()));
 }
