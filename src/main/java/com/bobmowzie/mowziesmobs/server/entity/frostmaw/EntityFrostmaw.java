e9fb0afe297d343b092f19e067e802d36cbb73d7
BobMowzie <jnad325@gmail.com>
2021-05-05 02:01:16 -0400

    Buffed frostmaw

--- src/main/java/com/bobmowzie/mowziesmobs/server/entity/frostmaw/EntityFrostmaw.java
+++ src/main/java/com/bobmowzie/mowziesmobs/server/entity/frostmaw/EntityFrostmaw.java
@@ -441,7 +441,7 @@ public class EntityFrostmaw extends MowzieEntity implements IMob {
                     getNavigator().tryMoveToEntityLiving(getAttackTarget(), 0.85);
                 }
 
-                if (shouldDodgeMeasure >= 14) shouldDodge = true;
+                if (shouldDodgeMeasure >= 12) shouldDodge = true;
                 if (targetDistance < 4 && shouldDodge && getAnimation() == NO_ANIMATION) {
                     shouldDodge = false;
                     dodgeCooldown = DODGE_COOLDOWN;
@@ -463,7 +463,7 @@ public class EntityFrostmaw extends MowzieEntity implements IMob {
                         AnimationHandler.INSTANCE.sendAnimationMessage(this, SWIPE_TWICE_ANIMATION);
                     else AnimationHandler.INSTANCE.sendAnimationMessage(this, SWIPE_ANIMATION);
                 }
-                if ((targetDistance >= 3 || dodgeCooldown > DODGE_COOLDOWN - 20) && targetDistance <= 13.5 && getAnimation() == NO_ANIMATION && iceBreathCooldown <= 0 && getHasCrystal() && (onGround || inWater)) {
+                if (targetDistance <= 13.5 && getAnimation() == NO_ANIMATION && iceBreathCooldown <= 0 && getHasCrystal() && (onGround || inWater)) {
                     AnimationHandler.INSTANCE.sendAnimationMessage(this, ICE_BREATH_ANIMATION);
                     iceBreathCooldown = ICE_BREATH_COOLDOWN;
                 }
