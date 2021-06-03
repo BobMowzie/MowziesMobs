cc52256ee109ffcb65d7bcaa768dddd979cb22c0
BobMowzie <jnad325@gmail.com>
2021-06-03 16:47:33 -0400

    Whoops. Left Barako in permanent supernova by mistake

--- src/main/java/com/bobmowzie/mowziesmobs/server/entity/barakoa/EntityBarako.java
+++ src/main/java/com/bobmowzie/mowziesmobs/server/entity/barakoa/EntityBarako.java
@@ -469,9 +469,9 @@ public class EntityBarako extends MowzieEntity implements LeaderSunstrikeImmune,
             timeUntilSupernova--;
         }
 
-        if (getAnimation() == NO_ANIMATION) {
-            AnimationHandler.INSTANCE.sendAnimationMessage(this, SUPERNOVA_ANIMATION);
-        }
+//        if (getAnimation() == NO_ANIMATION) {
+//            AnimationHandler.INSTANCE.sendAnimationMessage(this, SUPERNOVA_ANIMATION);
+//        }
     }
 
     private void superNovaEffects() {
