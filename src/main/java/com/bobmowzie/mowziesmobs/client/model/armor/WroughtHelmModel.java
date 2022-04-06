448308e17c616963f81b7d4918f6fdff7d6c7595
BobMowzie <jnad325@gmail.com>
2022-04-06 11:47:30 -0700

    Shrank Wrought helm horns

--- src/main/java/com/bobmowzie/mowziesmobs/client/model/armor/WroughtHelmModel.java
+++ src/main/java/com/bobmowzie/mowziesmobs/client/model/armor/WroughtHelmModel.java
@@ -5,60 +5,69 @@ import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.entity.LivingEntity;
 
 public class WroughtHelmModel<T extends LivingEntity> extends BipedModel<T> {
-    public ModelRenderer shape1;
-    public ModelRenderer tuskRight1;
-    public ModelRenderer hornRight1;
-    public ModelRenderer tuskLeft1;
-    public ModelRenderer hornLeft1;
-    public ModelRenderer tuskRight2;
-    public ModelRenderer hornRight2;
-    public ModelRenderer tuskLeft2;
-    public ModelRenderer hornLeft2;
+    private final ModelRenderer shape1;
+    private final ModelRenderer tuskRight1;
+    private final ModelRenderer tuskRight2;
+    private final ModelRenderer hornRight1;
+    private final ModelRenderer hornLeft;
+    private final ModelRenderer tuskLeft1;
+    private final ModelRenderer tuskLeft2;
+    private final ModelRenderer hornLeft1;
+    private final ModelRenderer hornRight;
 
     public WroughtHelmModel() {
         super(0.0f);
         this.textureWidth = 64;
-        this.textureHeight = 64;
+        this.textureHeight = 32;
         bipedHead.cubeList.clear();
-        this.shape1 = new ModelRenderer(this, 0, 44);
-        this.shape1.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.shape1.addBox(-5.0F, -9.0F, -5.0F, 10, 10, 10, 0.0F);
-        this.tuskLeft1 = new ModelRenderer(this, 40, 55);
-        this.tuskLeft1.setRotationPoint(-3.0F, -1.0F, -3.0F);
-        this.tuskLeft1.addBox(-1.5F, -1.5F, -6.0F, 3, 3, 6, 0.0F);
-        this.setRotateAngle(tuskLeft1, 0.4363323129985824F, 0.7853981633974483F, 0.0F);
-        this.hornRight2 = new ModelRenderer(this, 34, 32);
-        this.hornRight2.setRotationPoint(-1.0F, 1.5F, -8.0F);
-        this.hornRight2.addBox(0.0F, -2.0F, -7.0F, 2, 2, 7, 0.0F);
-        this.setRotateAngle(hornRight2, -1.2217304763960306F, 0.0F, 0.0F);
-        this.tuskRight2 = new ModelRenderer(this, 34, 32);
-        this.tuskRight2.setRotationPoint(-1.0F, 1.5F, -6.0F);
-        this.tuskRight2.addBox(0.0F, -2.0F, -7.0F, 2, 2, 7, 0.0F);
-        this.setRotateAngle(tuskRight2, -0.8726646259971648F, 0.0F, 0.0F);
-        this.hornLeft1 = new ModelRenderer(this, 8, 33);
-        this.hornLeft1.setRotationPoint(-3.0F, -7.0F, -3.0F);
-        this.hornLeft1.addBox(-1.5F, -1.5F, -8.0F, 3, 3, 8, 0.0F);
-        this.setRotateAngle(hornLeft1, -0.3490658503988659F, 0.7853981633974483F, 0.0F);
-        this.tuskRight1 = new ModelRenderer(this, 40, 55);
-        this.tuskRight1.setRotationPoint(3.0F, -1.0F, -3.0F);
-        this.tuskRight1.addBox(-1.5F, -1.5F, -6.0F, 3, 3, 6, 0.0F);
-        this.setRotateAngle(tuskRight1, 0.4363323129985824F, -0.7853981633974483F, 0.0F);
-        this.hornLeft2 = new ModelRenderer(this, 30, 41);
-        this.hornLeft2.setRotationPoint(-1.0F, 1.5F, -8.0F);
-        this.hornLeft2.addBox(0.0F, -2.0F, -11.0F, 2, 2, 11, 0.0F);
-        this.setRotateAngle(hornLeft2, -1.2217304763960306F, 0.0F, 0.0F);
-        this.tuskLeft2 = new ModelRenderer(this, 34, 32);
-        this.tuskLeft2.setRotationPoint(-1.0F, 1.5F, -6.0F);
-        this.tuskLeft2.addBox(0.0F, -2.0F, -7.0F, 2, 2, 7, 0.0F);
-        this.setRotateAngle(tuskLeft2, -0.8726646259971648F, 0.0F, 0.0F);
-        this.hornRight1 = new ModelRenderer(this, 8, 33);
-        this.hornRight1.setRotationPoint(3.0F, -7.0F, -3.0F);
-        this.hornRight1.addBox(-1.5F, -1.5F, -8.0F, 3, 3, 8, 0.0F);
-        this.setRotateAngle(hornRight1, -0.3490658503988659F, -0.7853981633974483F, 0.0F);
-        this.hornRight1.addChild(this.hornRight2);
-        this.tuskRight1.addChild(this.tuskRight2);
-        this.hornLeft1.addChild(this.hornLeft2);
-        this.tuskLeft1.addChild(this.tuskLeft2);
+
+        shape1 = new ModelRenderer(this);
+        shape1.setRotationPoint(0.0F, 0.0F, 0.0F);
+        shape1.setTextureOffset(0, 12).addBox(-5.0F, -10.0F, -5.0F, 10.0F, 10.0F, 10.0F, 0.0F, false);
+
+        tuskRight1 = new ModelRenderer(this);
+        tuskRight1.setRotationPoint(-2.5F, -1.5F, -2.5F);
+        setRotationAngle(tuskRight1, 0.4363F, 0.7854F, 0.0F);
+        tuskRight1.setTextureOffset(40, 24).addBox(-1.5F, -1.5F, -5.0F, 3.0F, 3.0F, 5.0F, 0.0F, false);
+
+        tuskRight2 = new ModelRenderer(this);
+        tuskRight2.setRotationPoint(1.0F, 1.5F, -5.0F);
+        tuskRight1.addChild(tuskRight2);
+        setRotationAngle(tuskRight2, -0.8727F, 0.0F, 0.0F);
+        tuskRight2.setTextureOffset(34, 4).addBox(-2.0F, -2.0F, -5.0F, 2.0F, 2.0F, 5.0F, 0.0F, false);
+
+        hornRight1 = new ModelRenderer(this);
+        hornRight1.setRotationPoint(3.0F, -8.0F, -3.0F);
+        setRotationAngle(hornRight1, -0.3491F, -0.7854F, 0.0F);
+        hornRight1.setTextureOffset(8, 3).addBox(-1.5F, -1.5F, -6.0F, 3.0F, 3.0F, 6.0F, 0.0F, true);
+
+        hornLeft = new ModelRenderer(this);
+        hornLeft.setRotationPoint(-1.0F, 1.5F, -6.0F);
+        hornRight1.addChild(hornLeft);
+        setRotationAngle(hornLeft, -1.2217F, 0.0F, 0.0F);
+        hornLeft.setTextureOffset(43, 12).addBox(0.0F, -2.0F, -6.0F, 2.0F, 2.0F, 6.0F, 0.0F, true);
+
+        tuskLeft1 = new ModelRenderer(this);
+        tuskLeft1.setRotationPoint(2.5F, -1.5F, -2.5F);
+        setRotationAngle(tuskLeft1, 0.4363F, -0.7854F, 0.0F);
+        tuskLeft1.setTextureOffset(40, 24).addBox(-1.5F, -1.5F, -5.0F, 3.0F, 3.0F, 5.0F, 0.0F, false);
+
+        tuskLeft2 = new ModelRenderer(this);
+        tuskLeft2.setRotationPoint(1.0F, 1.5F, -5.0F);
+        tuskLeft1.addChild(tuskLeft2);
+        setRotationAngle(tuskLeft2, -0.8727F, 0.0F, 0.0F);
+        tuskLeft2.setTextureOffset(34, 4).addBox(-2.0F, -2.0F, -5.0F, 2.0F, 2.0F, 5.0F, 0.0F, false);
+
+        hornLeft1 = new ModelRenderer(this);
+        hornLeft1.setRotationPoint(-3.0F, -8.0F, -3.0F);
+        setRotationAngle(hornLeft1, -0.3491F, 0.7854F, 0.0F);
+        hornLeft1.setTextureOffset(8, 3).addBox(-1.5F, -1.5F, -6.0F, 3.0F, 3.0F, 6.0F, 0.0F, true);
+
+        hornRight = new ModelRenderer(this);
+        hornRight.setRotationPoint(-1.0F, 1.5F, -6.0F);
+        hornLeft1.addChild(hornRight);
+        setRotationAngle(hornRight, -1.2217F, 0.0F, 0.0F);
+        hornRight.setTextureOffset(30, 12).addBox(0.0F, -2.0F, -8.0F, 2.0F, 2.0F, 8.0F, 0.0F, true);
 
         this.bipedHead.addChild(this.shape1);
         this.bipedHead.addChild(this.hornLeft1);
@@ -67,10 +76,7 @@ public class WroughtHelmModel<T extends LivingEntity> extends BipedModel<T> {
         this.bipedHead.addChild(this.tuskRight1);
     }
 
-    /**
-     * This is a helper function from Tabula to set the rotation of model parts
-     */
-    public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+    public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
         modelRenderer.rotateAngleX = x;
         modelRenderer.rotateAngleY = y;
         modelRenderer.rotateAngleZ = z;
