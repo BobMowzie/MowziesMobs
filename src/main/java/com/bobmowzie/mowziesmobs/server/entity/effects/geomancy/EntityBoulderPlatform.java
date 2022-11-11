a2b0664a23f92363c04aca9b4ab7f840661dc273
BobMowzie <jnad325@gmail.com>
2022-11-11 15:58:42 -0800

    Code cleanup

--- src/main/java/com/bobmowzie/mowziesmobs/server/entity/effects/geomancy/EntityBoulderPlatform.java
+++ src/main/java/com/bobmowzie/mowziesmobs/server/entity/effects/geomancy/EntityBoulderPlatform.java
@@ -21,6 +21,8 @@ public class EntityBoulderPlatform extends EntityBoulderBase {
     private static final int MAX_TRIES = 10;
 
     private EntityBoulderPlatform nextBoulder;
+    private EntitySculptor sculptor;
+    private EntityPillar pillar;
 
     protected boolean isMainPath = false;
 
@@ -40,140 +42,111 @@ public class EntityBoulderPlatform extends EntityBoulderBase {
     @Override
     public void tick() {
         super.tick();
-        if (tickTimer() % 5 == 0) {
-            checkJumpPath(this);
-        }
-        if (tickTimer() == 7 && !level.isClientSide()) {
-            nextBoulders();
-        }
-
-        if (caster instanceof EntitySculptor && !caster.isRemoved()) {
-            EntitySculptor sculptor = (EntitySculptor) caster;
-            EntityPillar pillar = sculptor.getPillar();
-            if (pillar == null || pillar.isRemoved()) {
-                this.explode();
+        if (tickTimer() == 1) {
+            if (caster instanceof EntitySculptor) {
+                sculptor = (EntitySculptor) caster;
+                pillar = sculptor.getPillar();
             }
         }
-        else {
-            this.explode();
+        if (sculptor == null || sculptor.isRemoved() || pillar == null || pillar.isRemoved()) {
+            remove(RemovalReason.DISCARDED);
+            return;
+        }
+
+        if (tickTimer() == 7 && !level.isClientSide()) {
+            nextBoulders();
         }
     }
 
     public void nextBoulders() {
-        if (caster instanceof EntitySculptor) {
-            EntitySculptor sculptor = (EntitySculptor) caster;
-            EntityPillar pillar = sculptor.getPillar();
-            if (pillar != null) {
+        if (!isMainPath && random.nextFloat() > 0.75) return;
 
-                if (!isMainPath && random.nextFloat() > 0.75) return;
-
-                int numNextBoulders = (int) (Math.pow(random.nextFloat(), 16) * 3) + 1;
-                for (int i = 0; i < numNextBoulders; i++) {
-                    nextSingleBoulder();
-                }
-            }
+        int numNextBoulders = (int) (Math.pow(random.nextFloat(), 16) * 3) + 1;
+        for (int i = 0; i < numNextBoulders; i++) {
+            nextSingleBoulder();
         }
     }
 
     public void nextSingleBoulder() {
-        if (caster instanceof EntitySculptor) {
-            EntitySculptor sculptor = (EntitySculptor) caster;
-            EntityPillar pillar = sculptor.getPillar();
-            if (pillar != null) {
-                int baseHeight = (int) sculptor.getPillar().position().y;
-                int whichTierIndex = (int) (Math.pow(random.nextFloat(), 2) * (GeomancyTier.values().length - 2) + 1);
-                GeomancyTier nextTier = GeomancyTier.values()[whichTierIndex];
-                EntityBoulderPlatform nextBoulder = new EntityBoulderPlatform(EntityHandler.BOULDER_PLATFORM.get(), getLevel(), caster, getBlock(), blockPosition(), nextTier);
+        int whichTierIndex = (int) (Math.pow(random.nextFloat(), 2) * (GeomancyTier.values().length - 2) + 1);
+        GeomancyTier nextTier = GeomancyTier.values()[whichTierIndex];
+        EntityBoulderPlatform nextBoulder = new EntityBoulderPlatform(EntityHandler.BOULDER_PLATFORM.get(), getLevel(), caster, getBlock(), blockPosition(), nextTier);
 
-                for (int j = 0; j < MAX_TRIES; j++) {
-                    Vec3 randomPos;
-                    if (position().y() < baseHeight + EntitySculptor.TEST_HEIGHT) {
-                        randomPos = chooseRandomLocation(nextBoulder);
-                    }
-                    // If the platform is already at max height, next platform should move towards sculptor
-                    else if (position().multiply(1, 0, 1).distanceTo(sculptor.position().multiply(1, 0, 1)) > MAX_DIST_HORIZONTAL) {
-                        randomPos = chooseTowardsSculptorLocation(nextBoulder);
-                    } else return;
-                    nextBoulder.setPos(randomPos);
+        for (int j = 0; j < MAX_TRIES; j++) {
+            Vec3 randomPos;
+            if (getHeightFrac() < 1f) {
+                randomPos = chooseRandomLocation(nextBoulder);
+            }
+            // If the platform is already at max height, next platform should move towards sculptor
+            else if (position().multiply(1, 0, 1).distanceTo(sculptor.position().multiply(1, 0, 1)) > MAX_DIST_HORIZONTAL) {
+                randomPos = chooseTowardsSculptorLocation(nextBoulder);
+            } else return;
+            nextBoulder.setPos(randomPos);
 
-                    if (level.noCollision(nextBoulder)) {
-                        AABB toCheck = nextBoulder.getBoundingBox().inflate(MAX_DIST_HORIZONTAL, MAX_DIST_VERTICAL / 2f + 1.5f, MAX_DIST_HORIZONTAL).move(0, -MAX_DIST_VERTICAL / 2f - 1.5f, 0);
-                        List<EntityBoulderPlatform> platforms = level.getEntitiesOfClass(EntityBoulderPlatform.class, toCheck);
-                        boolean obstructsPath = false;
-                        for (EntityBoulderPlatform platform : platforms) {
-                            if (platform != nextBoulder && !nextBoulder.checkJumpPath(platform)) {
-                                obstructsPath = true;
-                                break;
-                            }
-                        }
-                        if (!obstructsPath) {
-                            getLevel().addFreshEntity(nextBoulder);
-                            if (isMainPath && this.nextBoulder == null) {
-                                this.nextBoulder = nextBoulder;
-                                this.nextBoulder.setMainPath();
-                            }
-                            return;
-                        }
+            // Make sure boulder has no collision
+            if (level.noCollision(nextBoulder)) {
+                // Check nearby boulders below to make sure this boulder doesn't block jumping path
+                AABB toCheck = nextBoulder.getBoundingBox().inflate(MAX_DIST_HORIZONTAL, MAX_DIST_VERTICAL / 2f + 1.5f, MAX_DIST_HORIZONTAL).move(0, -MAX_DIST_VERTICAL / 2f - 1.5f, 0);
+                List<EntityBoulderPlatform> platforms = level.getEntitiesOfClass(EntityBoulderPlatform.class, toCheck);
+                boolean obstructsPath = false;
+                for (EntityBoulderPlatform platform : platforms) {
+                    if (platform != nextBoulder && !nextBoulder.checkJumpPath(platform)) {
+                        obstructsPath = true;
+                        break;
                     }
+                }
+                if (!obstructsPath) {
+                    getLevel().addFreshEntity(nextBoulder);
+                    if (isMainPath && this.nextBoulder == null) {
+                        this.nextBoulder = nextBoulder;
+                        this.nextBoulder.setMainPath();
+                    }
+                    return;
                 }
             }
         }
     }
 
     protected Vec3 chooseRandomLocation(EntityBoulderPlatform nextBoulder) {
-        if (caster instanceof EntitySculptor) {
-            EntitySculptor sculptor = (EntitySculptor) caster;
-            EntityPillar pillar = sculptor.getPillar();
-            if (pillar != null) {
-                EntityDimensions thisDims = SIZE_MAP.get(this.getTier());
-                EntityDimensions nextDims = SIZE_MAP.get(nextBoulder.getTier());
-                Vec3 startLocation = position();
-                Vec2 fromPillarPos = new Vec2((float) (caster.getX() - startLocation.x), (float) (caster.getZ() - startLocation.z));
-                float horizontalOffset = random.nextFloat(1, MAX_DIST_HORIZONTAL) + thisDims.width/2f + nextDims.width/2f;
-                float verticalOffset = random.nextFloat(0, MAX_DIST_VERTICAL) - (nextDims.height - thisDims.height);
+        EntityDimensions thisDims = SIZE_MAP.get(this.getTier());
+        EntityDimensions nextDims = SIZE_MAP.get(nextBoulder.getTier());
+        Vec3 startLocation = position();
+        Vec2 fromPillarPos = new Vec2((float) (caster.getX() - startLocation.x), (float) (caster.getZ() - startLocation.z));
+        float horizontalOffset = random.nextFloat(1, MAX_DIST_HORIZONTAL) + thisDims.width/2f + nextDims.width/2f;
+        float verticalOffset = random.nextFloat(0, MAX_DIST_VERTICAL) - (nextDims.height - thisDims.height);
 
-                float baseAngle = (float) -Math.toDegrees(Math.atan2(fromPillarPos.y, fromPillarPos.x));
-                // Minimum and maximum angles force the angle to approach 90 degrees as it gets too close or too far from the pillar
-                float minRandomAngle = (float) (Math.min(Math.pow(3f, -fromPillarPos.length() + 3), 1f) * 90f);
-                float maxRandomAngle = 180f - (float) (Math.min(Math.pow(3f, fromPillarPos.length() - EntitySculptor.TEST_RADIUS), 1f) * 90f);
-                float randomAngle = random.nextFloat(minRandomAngle, maxRandomAngle);
-                if (random.nextBoolean()) randomAngle *= -1;
-                randomAngle *= 1f - Math.pow((startLocation.y() - pillar.getY()) / EntitySculptor.TEST_HEIGHT, 5f);
-                Vec3 offset = new Vec3(horizontalOffset, verticalOffset, 0);
-                float finalAngle = (float) Math.toRadians(MathHelper.wrapDegrees(baseAngle + randomAngle));
-                offset = offset.yRot(finalAngle);
-                Vec3 nextLocation = startLocation.add(offset);
-                if (nextLocation.y() + nextDims.height > pillar.getY() + EntitySculptor.TEST_HEIGHT) {
-                    nextLocation = new Vec3(nextLocation.x(), pillar.getY() + EntitySculptor.TEST_HEIGHT - nextDims.height, nextLocation.z());
-                }
-
-                return nextLocation;
-            }
+        float baseAngle = (float) -Math.toDegrees(Math.atan2(fromPillarPos.y, fromPillarPos.x));
+        // Minimum and maximum angles force the angle to approach 90 degrees as it gets too close or too far from the pillar
+        float minRandomAngle = (float) (Math.min(Math.pow(3f, -fromPillarPos.length() + 3), 1f) * 90f);
+        float maxRandomAngle = 180f - (float) (Math.min(Math.pow(3f, fromPillarPos.length() - EntitySculptor.TEST_RADIUS), 1f) * 90f);
+        float randomAngle = random.nextFloat(minRandomAngle, maxRandomAngle);
+        if (random.nextBoolean()) randomAngle *= -1;
+        randomAngle *= 1f - Math.pow(getHeightFrac(), 5f);
+        Vec3 offset = new Vec3(horizontalOffset, verticalOffset, 0);
+        float finalAngle = (float) Math.toRadians(MathHelper.wrapDegrees(baseAngle + randomAngle));
+        offset = offset.yRot(finalAngle);
+        Vec3 nextLocation = startLocation.add(offset);
+        if (nextLocation.y() + nextDims.height > pillar.getY() + EntitySculptor.TEST_HEIGHT) {
+            nextLocation = new Vec3(nextLocation.x(), pillar.getY() + EntitySculptor.TEST_HEIGHT - nextDims.height, nextLocation.z());
         }
-        return position();
+
+        return nextLocation;
     }
 
     // For when the platforms have reached max height and just need to head towards sculptor
     protected Vec3 chooseTowardsSculptorLocation(EntityBoulderPlatform nextBoulder) {
-        if (caster instanceof EntitySculptor) {
-            EntitySculptor sculptor = (EntitySculptor) caster;
-            EntityPillar pillar = sculptor.getPillar();
-            if (pillar != null) {
-                EntityDimensions thisDims = SIZE_MAP.get(this.getTier());
-                EntityDimensions nextDims = SIZE_MAP.get(nextBoulder.getTier());
-                Vec3 startLocation = position();
-                Vec2 fromPillarPos = new Vec2((float) (caster.getX() - startLocation.x), (float) (caster.getZ() - startLocation.z));
-                float horizontalOffset = random.nextFloat(1, MAX_DIST_HORIZONTAL) + thisDims.width/2f + nextDims.width/2f;
+        EntityDimensions thisDims = SIZE_MAP.get(this.getTier());
+        EntityDimensions nextDims = SIZE_MAP.get(nextBoulder.getTier());
+        Vec3 startLocation = position();
+        Vec2 fromPillarPos = new Vec2((float) (caster.getX() - startLocation.x), (float) (caster.getZ() - startLocation.z));
+        float horizontalOffset = random.nextFloat(1, MAX_DIST_HORIZONTAL) + thisDims.width/2f + nextDims.width/2f;
 
-                float baseAngle = (float) -Math.toDegrees(Math.atan2(fromPillarPos.y, fromPillarPos.x));
-                Vec3 offset = new Vec3(horizontalOffset, 0, 0);
-                float finalAngle = (float) Math.toRadians(MathHelper.wrapDegrees(baseAngle));
-                offset = offset.yRot(finalAngle);
+        float baseAngle = (float) -Math.toDegrees(Math.atan2(fromPillarPos.y, fromPillarPos.x));
+        Vec3 offset = new Vec3(horizontalOffset, 0, 0);
+        float finalAngle = (float) Math.toRadians(MathHelper.wrapDegrees(baseAngle));
+        offset = offset.yRot(finalAngle);
 
-                return startLocation.add(offset);
-            }
-        }
-        return position();
+        return startLocation.add(offset);
     }
 
     protected Vec3 startLocation() {
@@ -211,7 +184,6 @@ public class EntityBoulderPlatform extends EntityBoulderBase {
             Vec3 jumpPosition = new Vec3(0, gravity * time * time, 0).add(jumpVel.scale(time)).add(startPos);
             AABB playerBounds = EntityType.PLAYER.getDimensions().makeBoundingBox(jumpPosition);
             if (thisBounds.intersects(playerBounds)) return false;
-//            new TestEntity(EntityHandler.TEST_ENTITY, level, )
         }
 
         return true;
@@ -219,5 +191,16 @@ public class EntityBoulderPlatform extends EntityBoulderBase {
 
     public void setMainPath() {
         isMainPath = true;
+    }
+
+    public float getHeightFrac() {
+        if (caster instanceof EntitySculptor) {
+            EntitySculptor sculptor = (EntitySculptor) caster;
+            EntityPillar pillar = sculptor.getPillar();
+            if (pillar != null) {
+                return (float) (position().y() + getBbHeight() - pillar.getY()) / EntitySculptor.TEST_HEIGHT;
+            }
+        }
+        return -1;
     }
 }
