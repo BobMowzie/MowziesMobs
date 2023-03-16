93d4ca05187a59552e4e232e2a904bd514abc045
BobMowzie <jnad325@gmail.com>
2023-03-16 01:04:26 -0700

    Fixed base decoration on rooms and towers

---
+++ src/main/java/com/bobmowzie/mowziesmobs/server/world/feature/structure/processor/BaseDecoProcessor.java
@@ -0,0 +1,92 @@
+package com.bobmowzie.mowziesmobs.server.world.feature.structure.processor;
+
+import com.mojang.serialization.Codec;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.WorldGenRegion;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Half;
+import net.minecraft.world.level.block.state.properties.SlabType;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessor;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorType;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+
+import java.util.Random;
+
+public class BaseDecoProcessor extends StructureProcessor {
+    public static final BaseDecoProcessor INSTANCE = new BaseDecoProcessor();
+    public static final Codec<BaseDecoProcessor> CODEC = Codec.unit(() -> INSTANCE);
+
+    private static final BlockState trapDoorBottom = Blocks.DARK_OAK_TRAPDOOR.defaultBlockState();
+    private static final BlockState trapDoorTop = Blocks.DARK_OAK_TRAPDOOR.defaultBlockState().setValue(TrapDoorBlock.HALF, Half.TOP);
+    private static final BlockState slabBottom = Blocks.DARK_OAK_SLAB.defaultBlockState();
+    private static final BlockState slabTop = Blocks.DARK_OAK_SLAB.defaultBlockState().setValue(SlabBlock.TYPE, SlabType.TOP);
+    private static final BlockState woodStairs = Blocks.DARK_OAK_STAIRS.defaultBlockState().setValue(StairBlock.HALF, Half.TOP);
+    private static final BlockState wall = Blocks.DEEPSLATE_BRICK_WALL.defaultBlockState();
+    private static final BlockState button = Blocks.DARK_OAK_BUTTON.defaultBlockState();
+    private static final BlockState stoneStairs = Blocks.COBBLED_DEEPSLATE_STAIRS.defaultBlockState();
+
+    private static final BlockState[][] DECO = {
+            {trapDoorBottom, slabBottom, trapDoorBottom, slabBottom, trapDoorBottom, slabBottom, trapDoorBottom},
+            {woodStairs, trapDoorTop, slabTop, trapDoorTop, slabTop, trapDoorTop, woodStairs},
+            {wall, button, null, null, null, button, wall},
+            {wall, stoneStairs, stoneStairs, stoneStairs, stoneStairs, stoneStairs, wall}
+    };
+
+    protected StructureProcessorType<?> getType() {
+        return ProcessorHandler.BASE_DECO_PROCESSOR;
+    }
+
+    @Override
+    public StructureTemplate.StructureBlockInfo process(LevelReader levelReader, BlockPos jigsawPiecePos, BlockPos jigsawPieceBottomCenterPos, StructureTemplate.StructureBlockInfo blockInfoLocal, StructureTemplate.StructureBlockInfo blockInfoGlobal, StructurePlaceSettings structurePlacementData, StructureTemplate template) {
+        if (blockInfoGlobal.state.is(Blocks.PURPUR_STAIRS)) {
+            if (levelReader instanceof WorldGenRegion worldGenRegion && !worldGenRegion.getCenter().equals(new ChunkPos(blockInfoGlobal.pos))) {
+                return blockInfoGlobal;
+            }
+
+            Direction facing = blockInfoGlobal.state.getValue(StairBlock.FACING).getOpposite();
+            facing = structurePlacementData.getRotation().rotate(facing);
+            Random random = structurePlacementData.getRandom(blockInfoGlobal.pos);
+
+            blockInfoGlobal = new StructureTemplate.StructureBlockInfo(blockInfoGlobal.pos, Blocks.RED_TERRACOTTA.defaultBlockState(), blockInfoGlobal.nbt);
+            for (int x = 0; x < 7; x++) {
+                for (int y = 0; y < 4; y++) {
+                    BlockState state = DECO[y][x];
+                    if (state == null) continue;
+                    if (state.getBlock() == Blocks.COBBLED_DEEPSLATE_STAIRS) {
+                        state = chooseRandomState(random);
+                    }
+
+                    BlockPos pos = blockInfoGlobal.pos.relative(facing);
+                    pos = pos.relative(facing.getClockWise(), x - 3);
+                    pos = pos.relative(Direction.UP, 1 - y);
+
+                    if (levelReader.getBlockState(pos).getMaterial().isSolid()) continue;
+
+                    if (state.hasProperty(HorizontalDirectionalBlock.FACING)) {
+                        if (state.getBlock() instanceof StairBlock) {
+                            state = state.setValue(HorizontalDirectionalBlock.FACING, facing.getOpposite());
+                        }
+                        else {
+                            state = state.setValue(HorizontalDirectionalBlock.FACING, facing);
+                        }
+                    }
+                    levelReader.getChunk(pos).setBlockState(pos, state, false);
+                }
+            }
+        }
+        return blockInfoGlobal;
+    }
+
+    public BlockState chooseRandomState(Random random) {
+        float v = random.nextFloat();
+        if (v > 0.7) return Blocks.POLISHED_DEEPSLATE_STAIRS.defaultBlockState();
+        else return Blocks.COBBLED_DEEPSLATE_STAIRS.defaultBlockState();
+    }
+
+}
--- src/main/java/com/bobmowzie/mowziesmobs/server/world/feature/structure/processor/ProcessorHandler.java
+++ src/main/java/com/bobmowzie/mowziesmobs/server/world/feature/structure/processor/ProcessorHandler.java
@@ -9,11 +9,13 @@ public class ProcessorHandler {
     public static StructureProcessorType<BaseProcessor> BASE_PROCESSOR = () -> BaseProcessor.CODEC;
     public static StructureProcessorType<BlockSwapProcessor> BLOCK_SWAP_PROCESSOR = () -> BlockSwapProcessor.CODEC;
     public static StructureProcessorType<RootsProcessor> ROOTS_PROCESSOR = () -> RootsProcessor.CODEC;
+    public static StructureProcessorType<BaseDecoProcessor> BASE_DECO_PROCESSOR = () -> BaseDecoProcessor.CODEC;
 
     public static void registerStructureProcessors() {
         register("base_processor", BASE_PROCESSOR);
         register("block_swap_processor", BLOCK_SWAP_PROCESSOR);
         register("roots_processor", ROOTS_PROCESSOR);
+        register("base_deco_processor", BASE_DECO_PROCESSOR);
     }
 
     private static void register(String name, StructureProcessorType<?> codec) {
