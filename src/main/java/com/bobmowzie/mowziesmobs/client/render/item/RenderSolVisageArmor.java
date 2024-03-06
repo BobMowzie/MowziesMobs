dbe5d695643e1ccd0b0f643c598ff547915185bc
BobMowzie <jnad325@gmail.com>
2024-03-06 11:13:51 -0800

    Fix missing backface rendering on masks

--- src/main/java/com/bobmowzie/mowziesmobs/client/render/item/RenderSolVisageArmor.java
+++ src/main/java/com/bobmowzie/mowziesmobs/client/render/item/RenderSolVisageArmor.java
@@ -3,10 +3,22 @@ package com.bobmowzie.mowziesmobs.client.render.item;
 import com.bobmowzie.mowziesmobs.client.model.armor.SolVisageModel;
 import com.bobmowzie.mowziesmobs.client.render.entity.MowzieGeoArmorRenderer;
 import com.bobmowzie.mowziesmobs.server.item.ItemSolVisage;
+import com.bobmowzie.mowziesmobs.server.item.ItemUmvuthanaMask;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
 
 public class RenderSolVisageArmor extends MowzieGeoArmorRenderer<ItemSolVisage> {
 
     public RenderSolVisageArmor() {
         super(new SolVisageModel());
+    }
+
+    @Override
+    public RenderType getRenderType(ItemSolVisage animatable, float partialTick, PoseStack poseStack, @Nullable MultiBufferSource bufferSource, @Nullable VertexConsumer buffer, int packedLight, ResourceLocation texture) {
+        return RenderType.entityCutoutNoCull(texture);
     }
 }
